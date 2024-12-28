8eaad1b344ac67fe574679cebe0f31700b57cefe
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-12-28 17:02:58 +0200

    O(n) takes time which equal to the number of inputs.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public void log(int[] numbers) {
         // O(1) runs in constant time
         System.out.println(numbers[0]);
+        // O(n) takes time which equal to the number of inputs
+        for (int number : numbers) System.out.println(number);
     }
 }
