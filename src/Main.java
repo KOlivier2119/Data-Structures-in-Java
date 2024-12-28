706fcb3e1cac95e3cdd813519c454a4032a28e41
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-12-28 17:09:16 +0200

    O(n^2) for the nested loops

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         System.out.println(numbers[0]);
         // O(n) takes time which equal to the number of inputs
         for (int number : numbers) System.out.println(number);
+        // O(n^2) is for the nested loops which takes time which equals to the product of their inputs
+        for (int first : numbers)
+            for (int second : numbers)
+                for (int third : numbers) // O(n^3) since its the third loop
+                    System.out.println(first + "," + second + "," + third);
     }
 }
