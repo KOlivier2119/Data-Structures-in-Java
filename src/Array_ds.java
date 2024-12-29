0d6c8c87a307cd116d5547ae1ac85c8ad2c12dd4
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-12-29 07:23:35 +0200

    Array as data structure.

---
+++ src/Array_ds.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Array_ds {
+    public static void main(String[] args) {
+        int[] numbers = new int[3];
+        System.out.println(numbers.length);
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        System.out.println(Arrays.toString(numbers));
+    }
+}
--- src/Space.java
+++ src/Space.java
@@ -1,7 +1,7 @@
 public class Space {
     public void greet(String[] names) {
         String[] copy = new String[names.length];
-
+        // O(n)
         for (int i = 0; i < names.length; i++) {
             System.out.println("Hi " + names[i]);
         }
